b63dcd736d4b4209c7a0182a30c91d1b521648de
張語航 <00257122@ntou.edu.tw>
2016-04-25 15:15:42 +0800

    update

--- NicklineDetection.cpp
+++ NicklineDetection.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <cstdio>
 
-#define range 50
+#define range 30
 #define blue 75
 #define green 97
 #define red 125
@@ -22,16 +22,21 @@ Mat color(Mat& src) {
     inRange(src, Scalar(blue - range, green - range, red - range), Scalar(blue + range, green + range, red + range), src);
     return src;
 }
-
-
-
-bool color(double b, double g, double r) {
-    if ((b > blue - range && b < blue + range) && (g>green - range && g < green + range) && (r>red - range && r < red + range))
-        return true;
-    else
+bool color(Mat& frame,int row,int k){
+    int b=frame.at<Vec3b>(row, k)[0];
+    int g=frame.at<Vec3b>(row, k)[1];
+    int r=frame.at<Vec3b>(row, k)[2];
+    //cout<<(int)b<<" ";
+    if(b==0&&g==0&&r==0)
         return false;
+    else return true;
+    /*
+     if ((b > blue - range && b < blue + range) && (g>green - range && g < green + range) && (r>red - range && r < red + range))
+     return true;
+     else
+     return false;
+     */
 }
-
 
 int main(){
     VideoCapture cap(0);
@@ -39,27 +44,24 @@ int main(){
     namedWindow("Original", WINDOW_AUTOSIZE);
     namedWindow("Skin", WINDOW_AUTOSIZE);
     
-    int now_row = 87;
+    int now_row = 0;
     int breath=0;
     bool change = false;
     int row_max = 0;
     int row_min=8787;
     
     for (;;) {
-        Mat frame;
+        Mat frame,skin;
         cap >> frame;
-        
-        flip(frame, frame, 1);
-        
+        skin=frame;
+        color(skin);
+        //flip(frame, frame, 1);
         bool check = false;
         for (int row = frame.rows - 2; row >= 0; row--){//NO FIRST
             for (int col = frame.cols - 2; col >= 0; col--){
                 int count = 0;
                 for (int k = col; k > col - long_max && col>long_max; k--) {
-                    if (color(frame.at<Vec3b>(row, k)[0], frame.at<Vec3b>(row, k)[1], frame.at<Vec3b>(row,k)[2])){
-                        
-                        if (!color(frame.at<Vec3b>(row + 1, col)[0], frame.at<Vec3b>(row + 1, col)[1], frame.at<Vec3b>(row + 1, col)[2]))
-                            break;
+                    if (color(skin,row,k)){
                         count++;
                     }
                     else {
@@ -94,7 +96,7 @@ int main(){
                         if (change){
                             if (row_max - row_min > 5){
                                 breath++;
-                                cout << "©I§l¦¸¼Æ: " << breath / 2 << endl;
+                                cout << "呼吸次數: " << breath / 2 << endl;
                             }
                             row_max = 0;
                             row_min = 8787;
@@ -102,17 +104,14 @@ int main(){
                         }
                         //cout << "DOWN" << endl;
                     }
-                    else
-                        cout << "NO MOVE" << endl;
+                    else;
+                    //cout << "NO MOVE" << endl;
                     now_row = row;
                     break;
                 }
             }
-            if (check) {
-                break;
-            }
+            if (check) break;
         }
-        
         imshow("Original", frame);
         imshow("Skin", color(frame));
         if (waitKey(30) >= 0) break;
