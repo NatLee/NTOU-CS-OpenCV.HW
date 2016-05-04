9264c2212b0a204de439385cbddbc3ddb9dba557
Nat <vip3268@gmail.com>
2016-05-04 21:54:58 +0800

    Update skin_color.cpp

--- heartbeats/skin_color.cpp
+++ heartbeats/skin_color.cpp
@@ -1,14 +1,14 @@
-﻿#include "skin_color.hpp"
+#include "skin_color.hpp"
 
 Mat skincolor(const Mat& src) {
-	int LY = 0;
-	int HY = 255;
+	static int LY = 0;
+	static int HY = 255;
 
-	int LCr = 137;
-	int HCr = 177;
+	static int LCr = 137;
+	static int HCr = 177;
 
-	int LCb = 77;
-	int HCb = 127;
+	static int LCb = 77;
+	static int HCb = 127;
 
 	namedWindow("Control", 1);
 	cvCreateTrackbar("LowY", "Control", &LY, 255);
@@ -21,7 +21,7 @@ Mat skincolor(const Mat& src) {
 	Mat dst;
 	cvtColor(src, dst, cv::COLOR_BGR2YCrCb);
 	inRange(dst, Scalar(LY, LCr, LCb), Scalar(HY, HCr, HCb), dst);
-	//erode(dst, dst, Mat(), Point(-1, -1), 2);
-	//dilate(dst, dst, Mat(), Point(-1, -1), 2);
+	erode(dst, dst, Mat(), Point(-1, -1), 2);
+	dilate(dst, dst, Mat(), Point(-1, -1), 2);
 	return dst;
 }
