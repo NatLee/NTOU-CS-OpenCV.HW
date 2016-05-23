ef3cfcc0a45aed894316395fb89cf28ea2559ed5
WenShihKen <rpg0690@gmail.com>
2016-05-23 19:23:48 +0800

    update

--- TextDetection/TextDetection.cpp
+++ TextDetection/TextDetection.cpp
@@ -9,6 +9,9 @@
 using namespace std;
 using namespace cv;
 
+int threshval = 100;
+Mat input, src;
+
 long long int count_thres(Mat);
 long long int count_thres(Mat in1)
 {
@@ -21,15 +24,35 @@ long long int count_thres(Mat in1)
 	return temp / (in1.rows*in1.cols);
 }
 
+static void on_trackbar(int, void *)
+{
+	Mat bw = threshval < 128 ? (src < threshval) : (src > threshval);
+	Mat labelImage(src.size(), CV_32S);
+	int nLabels = connectedComponents(bw, labelImage, 8);
+	std::vector<Vec3b> colors(nLabels);
+	colors[0] = Vec3b(0, 0, 0);//background
+	for (int label = 1; label < nLabels; ++label) {
+		colors[label] = Vec3b((rand() & 255), (rand() & 255), (rand() & 255));
+	}
+	Mat dst(src.size(), CV_8UC3);
+	for (int r = 0; r < dst.rows; ++r) {
+		for (int c = 0; c < dst.cols; ++c) {
+			int label = labelImage.at<int>(r, c);
+			Vec3b &pixel = dst.at<Vec3b>(r, c);
+			pixel = colors[label];
+		}
+	}
+	imshow("Connected Components", dst);
+}
+
 int main()
 {
 	VideoCapture cap(0);
 	if (!cap.isOpened()) return -1;
 	for (;;) {
-		Mat input;
 		cap >> input;
 		resize(input, input, Size(input.cols / 2, input.rows / 2));
-		
+		input.copyTo(src);
 		cvtColor(input, input, COLOR_BGR2GRAY);
 
 		long long int thres = 0;
@@ -54,22 +77,20 @@ int main()
 		Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
 		dilate(input, input, element, Point(-1, -1), 1);
 
-		Rect temp;
-		vector<vector<Point>> contours; // Vector for storing contour
-		vector<Vec4i> hierarchy;
-		findContours(input, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // Find the contours in the images
+		//Rect temp;
+		//vector<vector<Point>> contours; // Vector for storing contour
+		//vector<Vec4i> hierarchy;
+		//findContours(input, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // Find the contours in the images
 
-		cout << contours.size() << endl;
-
-		for (int i = 0; i < contours.size(); i++) {
-			double area = contourArea(contours[i], false);
-			if (area >= 30) {
-				temp = boundingRect(contours[i]);
-				rectangle(input, temp, Scalar(0, 255, 0), 1, 8, 0);
-			}
-		}
-
-		imshow("thres", input);
+		//for (int i = 0; i < contours.size(); i++) {
+		//	double area = contourArea(contours[i], false);
+		//	if (area >= 30) {
+		//		temp = boundingRect(contours[i]);
+		//		rectangle(input, temp, Scalar(255, 0, 0), 1, 8, 0);
+		//	}
+		//}
+		createTrackbar("Threshold", "Connected Components", &threshval, 255, on_trackbar);
+		on_trackbar(threshval, 0);
 
 		if (waitKey(30) >= 0) break;
 	}
