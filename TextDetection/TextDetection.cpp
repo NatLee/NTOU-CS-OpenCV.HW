c21509f5cde71130ee309cc70c208f864232a8ae
Nat <vip3268@gmail.com>
2016-05-28 22:36:22 +0800

    Update TextDetection.cpp

--- TextDetection/TextDetection.cpp
+++ TextDetection/TextDetection.cpp
@@ -6,21 +6,54 @@ bool cmp(int a, int b)
 	return a > b;
 }
 
+Mat clearDots(Mat &src) {
+	for (int r = 0; r < src.rows; ++r) {
+		for (int c = 0; c < src.cols; ++c) {
+			int label = src.ptr<uchar>(r, c)[0];
+			if (label == 255) {
+				int count = 0;
+				for (int i = r - 2; i < r + 2 && i >= 0 && i < src.rows; i++) {
+					for (int j = c - 2; j < c + 2 && j >= 0 && j < src.cols; j++) {
+						if (src.ptr<uchar>(i, j)[0] == 255) count++;
+					}
+				}
+				if (count < 5)
+					src.ptr<uchar>(r, c)[0] = 0;
+			}
+		}
+	}
+	Mat element = getStructuringElement(MORPH_ELLIPSE, Size(1, 1));
+	erode(src, src, element, cv::Point(-1, -1), 2);
+	element = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(src, src, element, cv::Point(-1, -1), 1);
+	return src;
+}
+
+
 static Mat on_trackbar(int, void *, Mat &src, Mat &input)
 {
 	TEXTRecognizer textHandler;
 	textHandler.initialize();
 	double confidence = 0.0;
 	string str;
-	imshow("binary", src);
+	//imshow("binary", src);
 
 	Mat labelImage(src.size(), CV_32SC1);
+
+	src = clearDots(src);
+
+	imshow("clear", src);
+
+
+
+
+
 	int nLabels = connectedComponents(src, labelImage, 8);
 	cout << nLabels << endl;
 
 	map<int, Rect> contour;
 	vector<int>area_count;//counting the all contour average area, delete the biggest contour
-	
+
 	for (int r = 0; r < labelImage.rows; ++r) {//store top left point and bottom right point
 		for (int c = 0; c < labelImage.cols; ++c) {
 			int label = labelImage.at<int>(r, c);
@@ -36,6 +69,7 @@ static Mat on_trackbar(int, void *, Mat &src, Mat &input)
 		}
 		//cout << endl;
 	}
+
 	string tstring = "roi";
 
 	int area_ave = 0;//do average area
@@ -44,17 +78,17 @@ static Mat on_trackbar(int, void *, Mat &src, Mat &input)
 		area_count.push_back(area);
 	}
 	sort(area_count.begin(), area_count.end(), cmp);
-	for (int i = 0; i < nLabels / 2; i++) 
+	for (int i = 0; i < nLabels / 2; i++)
 		area_ave += area_count[i];
 	area_ave /= (nLabels / 2);//end
-	
+
 	int count = 1;
 	for (int i = 1; i < nLabels; i++) {//find text
 		int area = (contour[i].height - contour[i].y)*(contour[i].width - contour[i].x);
-		contour[i].width -= (contour[i].x-2);
-		contour[i].height -= (contour[i].y-2);
+		contour[i].width -= (contour[i].x );
+		contour[i].height -= (contour[i].y );
 		if (area >= area_ave - d&&area <= area_ave + d&&area>0) {//Set the area ,don't take if exceed the region
-			cout << contour[i].x << " " << contour[i].y << " " << contour[i].width << " " << contour[i].height << " " << area << endl;
+			//cout << contour[i].x << " " << contour[i].y << " " << contour[i].width << " " << contour[i].height << " " << area << endl;
 			stringstream ss;
 			ss << count++;
 			cout << tstring + ss.str();
@@ -78,7 +112,7 @@ void textDetection(Mat &input) {
 	adaptiveThreshold(src, src, maxVal, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY_INV, 11, 2);
 
 	Mat element = getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
-	//erode(src, src, element, cv::Point(-1, -1), 2);
+	//erode(src, src, element, cv::Point(-1, -1), 1);
 	//dilate(src, src, element, cv::Point(-1, -1), 1);
 	input = on_trackbar(100, 0, src, input);
 }
