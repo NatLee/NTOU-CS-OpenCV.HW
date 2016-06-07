96575d3443ce0ce8167bae8295e96d83aa5e85dc
FinianWu <apple@appletekiMacBook-Air-2.local>
2016-06-07 11:11:55 +0800

    update (cam)
    now it can work with your cam

--- TextDetection/main.cpp
+++ TextDetection/main.cpp
@@ -1,11 +1,23 @@
 #include "TextDetection.hpp"
 #include "TEXTRecognizer.h"
+#include "opencv2/opencv.hpp"
 int main()
 {
-    //read the image
-    Mat input = imread("bambo-zoo.jpg", 1);
-    textDetection(input);
-	imshow("image_show", input);
-    waitKey(0);
+    VideoCapture cap(0);
+    if (!cap.isOpened()) return -1;
+    for (;;) {
+        Mat frame, input;
+        cap >> frame;
+        //flip(frame, frame, 1);
+        frame.copyTo(input);
+        imshow("image_show", input);
+        if ((waitKey(1)) !=-1) {
+            textDetection(input);
+            imshow("image_show", input);
+            waitKey(0);
+            destroyAllWindows();
+        }
+    }
     return 0;
 }
+
