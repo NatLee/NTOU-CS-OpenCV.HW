2c61c567da6a5fbd8262141ceb185be5f327415d
WenShihKen <rpg0690@gmail.com>
2016-06-28 21:32:26 +0800

    Add another thinning code

--- TextDetection4Ways/Source.cpp
+++ TextDetection4Ways/Source.cpp
@@ -53,6 +53,50 @@ void ThinSubiteration1(Mat & pSrc, Mat & pDst) {
 	}
 }
 
+void ThinSubiteration2(Mat & pSrc, Mat & pDst) {
+	int rows = pSrc.rows;
+	int cols = pSrc.cols;
+	pSrc.copyTo(pDst);
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (pSrc.at<float>(i, j) == 1.0f) {
+				/// get 8 neighbors
+				/// calculate C(p)
+				int neighbor0 = (int)pSrc.at<float>(i - 1, j - 1);
+				int neighbor1 = (int)pSrc.at<float>(i - 1, j);
+				int neighbor2 = (int)pSrc.at<float>(i - 1, j + 1);
+				int neighbor3 = (int)pSrc.at<float>(i, j + 1);
+				int neighbor4 = (int)pSrc.at<float>(i + 1, j + 1);
+				int neighbor5 = (int)pSrc.at<float>(i + 1, j);
+				int neighbor6 = (int)pSrc.at<float>(i + 1, j - 1);
+				int neighbor7 = (int)pSrc.at<float>(i, j - 1);
+				int C = int(~neighbor1 & (neighbor2 | neighbor3)) +
+					int(~neighbor3 & (neighbor4 | neighbor5)) +
+					int(~neighbor5 & (neighbor6 | neighbor7)) +
+					int(~neighbor7 & (neighbor0 | neighbor1));
+				if (C == 1) {
+					/// calculate N
+					int N1 = int(neighbor0 | neighbor1) +
+						int(neighbor2 | neighbor3) +
+						int(neighbor4 | neighbor5) +
+						int(neighbor6 | neighbor7);
+					int N2 = int(neighbor1 | neighbor2) +
+						int(neighbor3 | neighbor4) +
+						int(neighbor5 | neighbor6) +
+						int(neighbor7 | neighbor0);
+					int N = min(N1, N2);
+					if ((N == 2) || (N == 3)) {
+						int E = (neighbor5 | neighbor6 | ~neighbor0) & neighbor7;
+						if (E == 0) {
+							pDst.at<float>(i, j) = 0.0f;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 
 void crossing(unsigned char* img, int width, int height, int *feature) {
 	memset(feature, 0, width*height);
@@ -216,7 +260,7 @@ int main()
 		threshold(roiSubtract, roiSubtract, level, 255, THRESH_BINARY);
 		rectangle(frame, position, Scalar(0, 255, 255), 1, 8, 0);
 
-		//fill 
+		//fill
 		Mat im_floodfill = roiSubtract.clone();
 		floodFill(im_floodfill, Point(0, 0), Scalar(255));
 		Mat im_floodfill_inv;
@@ -266,6 +310,7 @@ int main()
 			//cvLine(image, cvPoint(i, 0), cvPoint(i, featureMaxColRoi[i]), cvScalar(255, 255, 255));
 		}
 		ThinSubiteration1(maxColRoi, maxColRoi);
+		ThinSubiteration2(maxColRoi, maxColRoi);
 		if (maxColRoi.cols != 0 && maxColRoi.rows != 0)//avoid crashing
 			imshow("maxColRoi", maxColRoi);
 		else
